151fa92bbb4f4645d8c8b4cc426a31427cce2020
Balázs Rigó <rigo.balazs86@gmail.com>
2019-07-12 21:01:54 +0200

    section 3 video 4 kodok implementalva

---
+++ IntroductionTDD/OddService/ExampleServiceImplementation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddService
+{
+    public class ExampleServiceImplementation
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Third { get; set; }
+
+        public void IncreaseCounters(int number)
+        {
+            if (number > 3 && number <= 5)
+            {
+                First++;
+            }
+            else if (number > 5 && number <=7)
+            {
+                Second++;
+            }
+        }
+    }
+}
---
+++ IntroductionTDD/OddServiceTests/OddServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using OddService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OddServiceTests
+{
+    public class OddServiceTests
+    {        
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void IsOddMethod_OddNumbers_ReturnsTrue(int number)
+        {
+            var service = new OddServiceImplementation();
+            var result = service.IsOdd(number);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(20)]
+        [InlineData(18)]
+        public void IsOddMethod_EvenNumbers_ReturnsFalse(int number)
+        {
+            var service = new OddServiceImplementation();
+            var result = service.IsOdd(number);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IncreaseCounters_SmallerThanThree_NoCountersIncreased()
+        {
+            var service = new ExampleServiceImplementation();
+            service.IncreaseCounters(1);
+
+            Assert.Equal(0,service.First);
+            Assert.Equal(0,service.Second);
+            Assert.Equal(0,service.Third);
+        }
+
+        [Fact]
+        public void IncreaseCounters_BetweenThreeAndFive_FirstIncreased()
+        {
+            var service = new ExampleServiceImplementation();
+            service.IncreaseCounters(4);
+
+            Assert.Equal(1, service.First);
+            Assert.Equal(0, service.Second);
+            Assert.Equal(0, service.Third);
+        }
+
+        [Fact]
+        public void IncreaseCounters_BetweenFiveAndSeven_SecondIncreased()
+        {
+            var service = new ExampleServiceImplementation();
+            service.IncreaseCounters(6);
+
+            Assert.Equal(0, service.First);
+            Assert.Equal(1, service.Second);
+            Assert.Equal(0, service.Third);
+        }
+
+    }
+}
