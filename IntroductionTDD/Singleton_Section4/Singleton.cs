958c859ef244cc1067ef669841bef4dc42fbf535
Balázs Rigó <rigo.balazs86@gmail.com>
2019-07-13 13:18:21 +0200

    Section 4 Singleton kod implementalva

---
+++ IntroductionTDD/Singleton_Section4/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Section4
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton() { }
+
+        static Singleton()
+        {
+            _instance = new Singleton();
+        }
+
+        public static Singleton Instance
+        {
+            get { return _instance; }
+        }
+    }
+}
